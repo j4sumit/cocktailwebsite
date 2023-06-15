bddc6ebc051b234aab3b57255d72403ba8221fd1
Sumit Kumar <j4sumit@gmail.com>
2023-06-15 23:41:59 +0530

    redux setup has been done

--- src/Components/PageNotFound.js
+++ src/Components/PageNotFound.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Layout from "./Layout";
 
 const PageNotFound = () => {
+const logo = "https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1";
+
   return (
-    <div>PageNotFound</div>
+    <Layout>
+      <div className="container">
+        <img src={logo} alt="logo" />
+      </div>
+    </Layout>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from "react-redux";
+import Store from './Redux/Store';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={Store}>
     <Router>
     <App />
     </Router>
+    </Provider>
   </React.StrictMode>
 );
 
